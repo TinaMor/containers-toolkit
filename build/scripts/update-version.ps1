529797ed4147466248417a32dbeba815e0c6b4eb
TinaMor <christine.murimi@gmail.com>
2024-05-09 18:19:39 +0300

    Fix: coverage Error: No files found matching glob pattern.

--- build/scripts/update-version.ps1
+++ build/scripts/update-version.ps1
@@ -1,12 +1,35 @@
+###########################################################################
+#                                                                         #
+#   Copyright (c) Microsoft Corporation. All rights reserved.             #
+#                                                                         #
+#   This code is licensed under the MIT License (MIT).                    #
+#                                                                         #
+###########################################################################
+
+<#
+.SYNOPSIS
+Updates the version of the containers-toolkit module in the module manifest file.
+
+The script is a PowerShell script that takes two parameters: 
+.PARAMETER ManifestPath
+The path to the module manifest file.
+Defaults to the first containers-toolkit.psd1 file found in the repository.
+
+.PARAMETER ReleaseType
+The type of release to perform. 
+Defaults to 'patch'.
+
+#>
+
 [CmdletBinding()]
 param (
-    [Parameter(Mandatory = $true)]
+    [Parameter(Mandatory = $false)]
     [ValidateScript({ Test-Path $_ -PathType Leaf })]
-    [String]$ManifestPath,
+    [String]$ManifestPath = (Get-ChildItem -Recurse -Filter "containers-toolkit.psd1").FullName ,
 
     [Parameter(Mandatory = $false)]
     [ValidateSet('major', 'minor', 'patch')]
-    [String]$ReleaseType
+    [String]$ReleaseType = 'patch'
 )
 
 $Script:ManifestPath = $ManifestPath
@@ -44,14 +67,19 @@ function Get-NewVersion {
     return (New-Object Version -ArgumentList $major, $minor, $build).ToString()
 }
 
-$NewSemVer = Get-NewVersion 
-Write-Output "Updating module version to $NewSemVer"
-$Params = @{
-    Path          = $manifestPath
-    ModuleVersion = $NewSemVer
+function Update-CTKModuleManifest {
+    $NewSemVer = Get-NewVersion 
+    $Params = @{
+        Path          = $manifestPath
+        ModuleVersion = $NewSemVer
+    }
+    Update-ModuleManifest @Params
+
+    # Test the manifest script is valid
+    Test-ModuleManifest -Path $manifestPath | Out-Null
+    
+    return $NewSemVer
 }
-Update-ModuleManifest @Params
 
 
-# Test the manifest script is valid
-Test-ModuleManifest -Path $manifestPath
+Update-CTKModuleManifest
