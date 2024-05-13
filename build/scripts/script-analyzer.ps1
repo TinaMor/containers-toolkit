6626e89f3041fc4c685f731aca86e7832105ea26
TinaMor <christine.murimi@gmail.com>
2024-05-13 12:45:21 +0300

    Convert PSScriptAnalyzer to markdown

--- build/scripts/script-analyzer.ps1
+++ build/scripts/script-analyzer.ps1
@@ -7,22 +7,70 @@
 ###########################################################################
 
 # $ErrorActionPreference = 'Stop'
+function ConvertTo-MarkdownTable {
+    param (
+        [Parameter(ValueFromPipeline = $true)]
+        [PSObject]$InputObject
+    )
+
+    $table = "| Rule Name | Severity | Count |`n| -------- | ------- | ------- |"
+    foreach ($object in $InputObject) {
+        $count = $object.Count
+
+        $name = $summary[0].Name -split ","
+        $severity = $name[0].Trim()
+        $ruleName = $name[1].Trim()
+
+        $table += "`n| $ruleName | $severity | $count |"
+    }
+
+    return $table
+}
+
+function ConvertTo-MarkDown {
+    param (
+        [PSObject]$InputObject,
+        [String]$IssueCountString
+    )
+
+    $summary = $InputObject | Group-Object -Property Severity, RuleName -NoElement
+    $table = ConvertTo-MarkdownTable $summary
+
+    return @(
+        "### PSSciptAnalysis Report`n"
+        "*$IssueCountString*`n"
+        "$table"
+    ) -join ' '
+}
+
+function main {
+    $codeAnalysis = Invoke-ScriptAnalyzer -Path .\Containers-Toolkit\ -Recurse -ExcludeRule PSProvideCommentHelp
+
+    $lintIssues = $codeAnalysis | Where-Object { $_.Severity -notlike 'Error' }
+    $lintErrors = $codeAnalysis | Where-Object { $_.Severity -like '*Error' }
+
+    $IssueCountString = "$($lintIssues.Count) errors and $($lintIssues.Count) warnings found"
+    Write-Warning $IssueCountString
+
+    if ($lintErrors -or $lintIssues) {
+        Export-Clixml -Path 'psscriptanalysis.xml' -InputObject $codeAnalysis
+
+        # Convert to markdown 
+        Write-Output (ConvertTo-MarkDown -InputObject $codeAnalysis -IssueCountString $IssueCountString)
+    }
+    else {
+        Write-Output $IssueCountString
+    }
+
+    if ($lintErrors) {
+        Throw "$($lintErrors.Count) lint errors found"
+    }
+}
+
 
 if (!(Get-Module -Name PSScriptAnalyzer -ListAvailable)) {
     Install-Module -Name PSScriptAnalyzer -Scope CurrentUser
 }
 Import-Module -Name PSScriptAnalyzer -Force
 
-$codeAnalysis = Invoke-ScriptAnalyzer -Path .\Containers-Toolkit\ -Recurse -ExcludeRule PSProvideCommentHelp
-
-$lintIssues = $codeAnalysis | Where-Object { $_.Severity -notlike 'Error' }
-$lintErrors = $codeAnalysis | Where-Object { $_.Severity -like '*Error' }
-Write-Warning "$($lintIssues.Count) errors and $($lintIssues.Count) warnings found"
-
-if ($lintErrors -or $lintIssues) {
-    Export-Clixml -Path 'psscriptanalysis.xml' -InputObject $codeAnalysis
-}
-
-if ($lintErrors) {
-    Throw "$($lintErrors.Count) lint errors found"
-}
+main
